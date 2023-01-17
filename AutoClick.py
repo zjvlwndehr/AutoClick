956e9653858eb974bb110cd089dfb9bb804569c0
TaehyeonPark <53563433+zjvlwndehr@users.noreply.github.com>
2023-01-17 21:49:16 +0900

    Update AutoClick.py

--- AutoClick.py
+++ AutoClick.py
@@ -1,7 +1,8 @@
 import win32api, win32con
 from time import sleep
 
-interval = 0.054
+LInterval = 0.045
+#RInterval = 0.023
 
 def click(x,y, button = "left"):
     win32api.SetCursorPos((x,y))
@@ -9,18 +10,24 @@ def click(x,y, button = "left"):
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
         sleep(0.01)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-    # elif button == "right":
-    #     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
-    #     sleep(0.01)
-    #     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
+    elif button == "right":
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
+        sleep(0.01)
+        win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
 
 while True:
     x,y = win32api.GetCursorPos()
     
-    if win32api.GetAsyncKeyState(win32con.VK_XBUTTON2):
+    if win32api.GetAsyncKeyState(win32con.VK_XBUTTON2) or win32api.GetAsyncKeyState(win32con.VK_XBUTTON1):
         # if win32api.GetAsyncKeyState(win32con.VK_LBUTTON) != 0:
-        print(win32api.GetAsyncKeyState(win32con.VK_XBUTTON2))
-        print(win32api.GetAsyncKeyState(win32con.VK_LBUTTON))
+        # print(win32api.GetAsyncKeyState(win32con.VK_XBUTTON2))
+        # print(win32api.GetAsyncKeyState(win32con.VK_LBUTTON))
         click(x,y, button = "left")
-        sleep(interval)
+        sleep(LInterval)
+    # if win32api.GetAsyncKeyState(win32con.VK_XBUTTON1):
+    #     # print(win32api.GetAsyncKeyState(win32con.VK_XBUTTON2))
+    #     # print(win32api.GetAsyncKeyState(win32con.VK_LBUTTON))
+    #     click(x,y, button = "right")
+    #     sleep(RInterval)
+    
     
